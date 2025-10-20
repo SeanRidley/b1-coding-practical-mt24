052f7bb583c660160038ffb6846bb32667bb01f7
Sean Ridley <sean.ridley05@gmail.com>
2025-10-20 15:52:42 +0100

    "Filled in init.py file"

---
+++ uuv_mission/__init__.py
@@ -0,0 +1,4 @@
+from .dynamic import Submarine, Mission, ClosedLoop, Trajectory
+from .control import PDPIDController, MPCController
+
+__all__ = ['Submarine', 'Mission', 'ClosedLoop', 'Trajectory', 'PDController', 'MPCController']
