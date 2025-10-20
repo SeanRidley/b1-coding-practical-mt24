3591e6a18b74de69ab2fc0f0eae3cd6afda162ce
Sean Ridley <sean.ridley05@gmail.com>
2025-10-20 15:44:17 +0100

    "Implemented controller"

---
+++ uuv_mission/control.py
@@ -0,0 +1,53 @@
+import numpy as np
+import cvxpy as cp
+
+class Controller:
+    def __init__(self, A: np.ndarray, B: np.ndarray, C: np.ndarray, D: np.ndarray):
+        self.A = A
+        self.B = B
+        self.C = C
+        self.D = D
+
+class PDController(Controller):
+    def __init__(self, A: np.ndarray, B: np.ndarray, C: np.ndarray, D: np.ndarray, kp: float = 0.15, kd: float = 0.6):
+        super().__init__(A, B, C, D)
+        self.kp = kp
+        self.kd = kd
+        self.prev_error = 0.0
+
+    def compute_action(self, x0: np.ndarray, reference: float) -> float:
+        
+        error = reference - self.C @ x0
+        derivative = (error - self.prev_error)
+
+        u = self.Kp * error + self.Kd * derivative
+        self.previous_error = error
+
+        return u.item()
+    
+class MPCController(Controller):
+    def __init__(self, A: np.ndarray, B: np.ndarray, C: np.ndarray, D: np.ndarray, horizon: int = 10):
+        super().__init__(A, B, C, D)
+        self.horizon = horizon
+        self.Q = Q
+        self.R = R
+
+    def compute_action(self, x0: np.ndarray, reference: np.ndarray) -> np.ndarray:
+        x = cp.Variable((self.A.shape[0], self.horizon + 1))
+        u = cp.Variable((self.B.shape[1], self.horizon))
+        y = cp.Variable((self.C.shape[0], self.horizon + 1))
+
+        cost = 0
+        constraints = [x[:, 0] == x0]
+
+        for t in range(self.horizon):
+            cost += cp.quad_form(y[:, t] - reference[;, t], self.Q) + cp.quad_form(u[:, t], self.R)
+            constraints += [
+                x[:, t + 1] == self.A @ x[:, t] + self.B @ u[:, t],
+                y[:, t] == self.C @ x[:, t] + self.D @ u[:, t]
+            ]
+
+        prob = cp.Problem(cp.Minimize(cost), constraints)
+        prob.solve()
+
+        return u.value[:, 0]
