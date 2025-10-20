f9380468168349864b8b2bf764512a20b7aa533c
Sean Ridley <sean.ridley05@gmail.com>
2025-10-20 17:27:50 +0100

    Further Changes for Kp

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -9,8 +9,10 @@ class Controller:
         self.D = D
 
 class PDController(Controller):
-    def __init__(self, A: np.ndarray, B: np.ndarray, C: np.ndarray, D: np.ndarray, Kp: float = 0.15, Kd: float = 0.6):
+    def __init__(self, A: np.ndarray, B: np.ndarray, C: np.ndarray, D: np.ndarray, Kp: float, Kd: float):
         super().__init__(A, B, C, D)
+        Kp = 0.15
+        Kd = 0.6
         self.Kp = Kp
         self.Kd = Kd
         self.prev_error = 0.0
